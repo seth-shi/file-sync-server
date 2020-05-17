cb7784b0d20db2e279f241eddec8c5bdb6f82429
david <1033404553@qq.com>
2020-05-17 13:36:23 +0800

    Added : 增加菜单

--- main.go
+++ main.go
@@ -38,6 +38,50 @@ func main() {
 		Title:   "SCREAMO",
 		MinSize: Size{600, 400},
 		Layout:  VBox{},
+		MenuItems: []MenuItem{
+			Menu{
+				Text: "&File",
+				Items: []MenuItem{
+					Action{
+						Text:        "&Open",
+						Shortcut:    Shortcut{walk.ModControl, walk.KeyO},
+						OnTriggered: func() {
+
+							fmt.Println("打开文件")
+						},
+					},
+					Separator{},
+					Action{
+						Text:        "E&xit",
+						OnTriggered: func() { mw.Close() },
+					},
+				},
+			},
+			Menu{
+				Text: "&View",
+				Items: []MenuItem{
+					Action{
+						Text:    "浏览",
+						OnTriggered: func() {
+
+							fmt.Println("浏览")
+						},
+					},
+				},
+			},
+			Menu{
+				Text: "&Help",
+				Items: []MenuItem{
+					Action{
+						Text:        "About",
+						OnTriggered: func() {
+
+							fmt.Println("帮助")
+						},
+					},
+				},
+			},
+		},
 		Children: []Widget{
 			PushButton{
 				Text: "SCREAM",
